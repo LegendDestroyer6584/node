f2ff8a5b5095d5ba71d2dc0ec02560d84f11d41f
LegendDestroyer6584 <89033134+LegendDestroyer6584@users.noreply.github.com>
2022-01-16 16:08:02 +0530

    async

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-console.log('hiya')
-console.log('secondthing')
-console.log('thirdth')
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+   if(req.url == '/'){
+     res.end('Site refused to connect')
+   }
+   if(req.url == '/about'){
+     // BLOCKING
+     for(let i = 0; i<100;i++){
+       for(let j = 0; j<1000;j++){
+         console.log(`${i}, ${j}`)
+       }
+     }
+     res.end("about")
+   }
+   res.end('not found')
+})
+
+server.listen(5000, ()=>{
+  console.log('Server listening on port 5000...');
+})
