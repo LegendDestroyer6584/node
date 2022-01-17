3c058fda92a7a380c548f949bb309eae379408b7
LegendDestroyer6584 <89033134+LegendDestroyer6584@users.noreply.github.com>
2022-01-17 10:56:53 +0530

    added content

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-const http = require('http');
+const {readFile} = require('fs')
 
+const getText = (path)=>{
+  return new Promise(function(resolve, reject) {
+    readFile('./content/first.txt', 'utf8',(err,data)=>{
+      if(err){
+        reject(err)
+      } else{
+        resolve(data)
+      }
+    })
 
-const server = http.createServer((req, res) => {
-   if(req.url == '/'){
-     res.end('Site refused to connect')
-   }
-   if(req.url == '/about'){
-     // BLOCKING
-     for(let i = 0; i<100;i++){
-       for(let j = 0; j<1000;j++){
-         console.log(`${i}, ${j}`)
-       }
-     }
-     res.end("about")
-   }
-   res.end('not found')
-})
+  });
+}
 
-server.listen(5000, ()=>{
-  console.log('Server listening on port 5000...');
-})
+getText('./content/first.txt')
