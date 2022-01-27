7f39c83b3dc2b10c144c3de1e489ed87e28f6e01
LegendDestroyer6584 <89033134+LegendDestroyer6584@users.noreply.github.com>
2022-01-27 16:34:45 +0530

    event emmiters enderstanding

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const EventEmmiter = require('events');
 const CustomEmmiter = new EventEmmiter();
 CustomEmmiter.on('response', () => {
-  console.log(`data recieved `)
+  console.log('data recieved ')
 })
-CustomEmmiter.emit('response')
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('What is the code?', ans => {
+  CustomEmmiter.emit(`${ans}`)
+  readline.close();
+});
